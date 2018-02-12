12000a5808f49e20b99fa6ca164c73d314115473
John Wesley M <jwes.devstudio@gmail.com>
2018-02-12 20:31:07 +0000

    Update Comm.php

--- helpers/Comm.php
+++ helpers/Comm.php
@@ -1,10 +1,10 @@
 <?php
 	function NewSQLConnection()
 	{		
-		$servername = "localhost";
-		$password = "Daniel@1997";
-		$username = "id3001818_admin";
-		$dbname = "id3001818_espirito";
+		$servername = "";
+		$password = "";
+		$username = "";
+		$dbname = "";
 
 		// $servername = "localhost";
 		// $password = "";
